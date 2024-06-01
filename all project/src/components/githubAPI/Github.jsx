e876af74eab5952de0a3ea923e68eceaadaa7312
Vishal D. <dwivedivishal0702@gmail.com>
2024-06-01 14:30:01 +0530

    add github profile or something idk

--- all project/src/components/githubAPI/Github.jsx
+++ all project/src/components/githubAPI/Github.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import Profile from "./Profile";
+//link =
 
 const Github = () => {
-  return <div>Github</div>;
+  const inputText = useRef();
+  const [name, setName] = useState("HummingCoder");
+  const [user, setuser] = useState({});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setName(inputText.current.value);
+  };
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${name}`)
+      .then((res) => res.json())
+      .then((data) => setuser(data))
+      .catch((err) => console.log(console.log(err)));
+  }, [name]);
+
+  return (
+    <div className="w-full h-screen grid grid-cols-1 place-content-center p-14 text-center">
+      <form
+        className="border-2 w-[400px] mx-auto flex justify-center items-center border-black rounded-lg overflow-hidden pl-2"
+        onSubmit={(e) => handleSubmit(e)}
+      >
+        <input
+          type="text"
+          className="w-full outline-none h-full"
+          ref={inputText}
+          placeholder="enter username..."
+        />
+        <button
+          className="bg-blue-500 py-1 text-white px-7 text-lg"
+          type="submit"
+        >
+          submit
+        </button>
+      </form>
+      <Profile {...user} />
+    </div>
+  );
 };
 
 export default Github;
--- all project/src/components/githubAPI/Profile.jsx
+++ all project/src/components/githubAPI/Profile.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import { FaArrowAltCircleRight, FaExternalLinkAlt } from "react-icons/fa";
 
-const Profile = () => {
-  return <div>Profile</div>;
+const Profile = ({
+  html_url,
+  avatar_url,
+  followers,
+  login,
+  name,
+  public_repos,
+}) => {
+  return (
+    <div className="w-[400px] relative h-fit bg-gray-100 shadow-lg mx-auto m-4 rounded-lg border-2 border-black overflow-hidden">
+      <img
+        className="w-[120px] shadow-lg mx-auto rounded-full border-[8px] m-2 border-white"
+        src={avatar_url}
+        alt=""
+      />
+      <div className=" p-5 mx-auto text-left  w-fit">
+        <p className="bg-white px-6 rounded-lg shadow-lg mb-1"> name: {name}</p>
+        <p className="bg-white px-6 rounded-lg shadow-lg mb-1">
+          username: {login}
+        </p>
+        <p className="bg-white px-6 rounded-lg shadow-lg mb-1">
+          followers: {followers}
+        </p>
+        <p className="bg-white px-6 rounded-lg shadow-lg mb-1">
+          public repos: {public_repos}
+        </p>
+      </div>
+      <a
+        className="absolute top-[100px] hover:bg-blue-600 left-[220px] bg-blue-500 text-white rounded-full  p-2"
+        target="_blank"
+        href={html_url}
+      >
+        <FaExternalLinkAlt />
+      </a>
+    </div>
+  );
 };
 
 export default Profile;
